0fb264883329da8091b4340c1808df19bc6a02b7
mahdi <mahdi@adhawkmicrosystems.com>
2023-11-16 17:36:26 -0500

    more pytests and basic github workflow for continuous testing

--- pyssp/optimal.py
+++ pyssp/optimal.py
@@ -70,33 +70,3 @@ def wiener_denoise():
 
 def wiener_systemid():
     pass
-
-
-if __name__ == "__main__":
-    import matplotlib.pyplot as plt
-
-    av = 1
-    aw = 0.36
-    A = 0.8
-    C = 1
-    N = 10
-
-    rng = np.random.default_rng(seed=100)
-
-    rw, rv = rng.spawn(2)
-
-    wn = rw.normal(0, aw, N)
-    vn = rv.normal(0, av, N)
-
-    xn = 0.8 * np.ones(N) + wn
-    yn = xn + vn
-
-    P0, P1, K, xhat0, xhat1 = kalman(yn, A, C, aw, av)
-
-    plt.plot(xn, label='x')
-    plt.plot(yn, label='y')
-    plt.plot(xhat0, label='xhat0')
-    plt.plot(xhat1, label='xhat1')
-
-    plt.legend()
-    plt.show()
---
+++ tests/test_optimal.py
@@ -0,0 +1,24 @@
+""""""
+
+import numpy as np
+from pyssp import optimal
+
+
+def test_kalman():
+    av = 1
+    aw = 0.36
+    A = 0.8
+    C = 1
+    N = 10
+
+    rng = np.random.default_rng(seed=100)
+
+    rw, rv = rng.spawn(2)
+
+    wn = rw.normal(0, aw, N)
+    vn = rv.normal(0, av, N)
+
+    xn = 0.8 * np.ones(N) + wn
+    yn = xn + vn
+
+    P0, P1, K, xhat0, xhat1 = optimal.kalman(yn, A, C, aw, av)
