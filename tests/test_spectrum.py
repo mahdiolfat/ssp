985babf12d635470191afe458b0a42c11d8fc6e7
Mahdi Olfat <mahdiolfat@gmail.com>
2024-02-28 17:46:32 -0500

    principle component estimators

---
+++ tests/test_spectrum.py
@@ -0,0 +1,30 @@
+"""
+Example 8.6.2: 8.31
+Frequency Estimators: Example 8.6.5. Page 468.
+"""
+
+from typing import NoReturn
+
+
+def test_phd() -> NoReturn:
+    raise NotImplementedError
+
+def test_music() -> NoReturn:
+    raise NotImplementedError
+
+# Frequency Estimators: Noise subspace methods
+def test_ev() -> NoReturn:
+    raise NotImplementedError
+
+def test_min_norm() -> NoReturn:
+    raise NotImplementedError
+
+# Principle component methods, Spectrum
+def test_bt_pc() -> NoReturn:
+    raise NotImplementedError
+
+def test_mv_pc() -> NoReturn:
+    raise NotImplementedError
+
+def test_ar_pc() -> NoReturn:
+    raise NotImplementedError
